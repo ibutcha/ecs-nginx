db26b5f12d3768c4cd6ca3efb2ffb76242b301bd
Butch Santos <icharliesantos12@gmail.com>
2020-07-28 16:05:43 +0800

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$(aws ecr get-login --region ap-southeast-2 --no-include-email)
+
+REPOSITORY_URI=$(aws ecr describe-repositories --repository-names nginx | grep repositoryUri | cut -d '"' -f 4)
+
+TAG=$(git rev-parse --short HEAD)
+
+docker build -t $REPOSITORY_URI:latest .
+
+docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$TAG
+
+docker push $REPOSITORY_URI
