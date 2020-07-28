3372b03d9358e0b32f98126886b6584144925bd1
Butch Santos <icharliesantos12@gmail.com>
2020-07-28 16:10:34 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,11 @@ $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 
 REPOSITORY_URI=$(aws ecr describe-repositories --repository-names nginx | grep repositoryUri | cut -d '"' -f 4)
 
-TAG=$(git rev-parse --short HEAD)
+IMAGE_TAG=$(git rev-parse --short HEAD)
 
 docker build -t $REPOSITORY_URI:latest .
 
-docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$TAG
+docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
 
-docker push $REPOSITORY_URI
+docker push $REPOSITORY_URI:latest
+docker push $REPOSITORY_URI:$IMAGE_TAG
